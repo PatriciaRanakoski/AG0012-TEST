80e89e110eb464400598d19351431b339d0e4f84
Patricia <ranakoskipatricia@gmail.com>
2025-10-18 12:32:10 +0100

    new test created

---
+++ mainChanges/v1.js
@@ -0,0 +1,11 @@
+let test1 = 'Hello AG0012';
+console.log(test1);
+
+let contact = 123
+
+if(test == contact){
+    console.log('You are right');
+} else {
+    console.log('You are wrong');
+}
+    
